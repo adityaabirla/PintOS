7101df7d3b721f67da9e096611edee60f5e4487e
Aditya Birla <adityabirla@Adityas-MacBook-Pro-3.local>
2025-08-25 13:49:09 +0530

    Implementing robust echo function with argument handling

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,13 +1,54 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <syscall.h>
+// int main(int argc, char **argv) {
+//   int i;
+//   if (argc == 1) {
+//     printf("\n");
+//     return EXIT_SUCCESS;
+//   }
+
+//   for (i = 1; i < argc; i++) 
+//   {
+//     int len = strlen(argv[i]);
+//     if (argv[i][0] == '\'') {
+//       argv[i][len - 1] = '\0';
+//     }
+//     argv[i]++;
+//     printf("%s ", argv[i]);
+//   }
+//   printf("\n");
+
+//   return EXIT_SUCCESS;
+// }
+
+
 #include <stdio.h>
 #include <syscall.h>
+#include <string.h>
 
-int
-main (int argc, char **argv)
+int main (int argc, char **argv)
 {
+  // printf("This is me changing the echo command yay\n");
   int i;
 
-  for (i = 0; i < argc; i++)
-    printf ("%s ", argv[i]);
+  if(argc == 1)
+  {
+    return EXIT_SUCCESS;
+  }
+
+
+  for (i = 1; i < argc; i++)
+    {
+      if(argv[i][0] == '\'')
+      {
+        //removal of the quotes
+        argv[i][strlen(argv[i]) - 1] = '\0';
+        argv[i]++; //move it one spot front so that we dont have to print that quote
+      }
+      printf ("%s ", argv[i]);
+    }
+
   printf ("\n");
 
   return EXIT_SUCCESS;
