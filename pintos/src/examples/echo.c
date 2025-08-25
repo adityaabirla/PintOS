82965c903719b98fcc733635837366e1833e4be2
Aditya Birla <adityabirla@Adityas-MacBook-Pro-3.local>
2025-08-25 13:52:29 +0530

    Implement robust echo command with argument handling

--- pintos/src/examples/echo.c
+++ pintos/src/examples/echo.c
@@ -1,28 +1,3 @@
-// #include <stdio.h>
-// #include <string.h>
-// #include <syscall.h>
-// int main(int argc, char **argv) {
-//   int i;
-//   if (argc == 1) {
-//     printf("\n");
-//     return EXIT_SUCCESS;
-//   }
-
-//   for (i = 1; i < argc; i++) 
-//   {
-//     int len = strlen(argv[i]);
-//     if (argv[i][0] == '\'') {
-//       argv[i][len - 1] = '\0';
-//     }
-//     argv[i]++;
-//     printf("%s ", argv[i]);
-//   }
-//   printf("\n");
-
-//   return EXIT_SUCCESS;
-// }
-
-
 #include <stdio.h>
 #include <syscall.h>
 #include <string.h>
