366005faca1cf99ab85e8065f2de667be08abae5
Aditya Birla <adityabirla@Adityas-MacBook-Pro-3.local>
2025-08-25 21:35:15 +0530

    implementing the touch command in pintos

--- pintos/src/examples/touch.c
+++ pintos/src/examples/touch.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <syscall.h>
+// #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    return 0;
+int main(int argc, char *argv[]) 
+{
+    for(int i = 1; i<argc; i++)
+    {
+        int flag = create(argv[i], 0);
+        if(!flag)
+        {
+            printf("%s: create failed\n", argv[i]);
+        }
+        else
+        {
+            printf("%s: created\n", argv[i]);
+        }
+    }
+    return EXIT_SUCCESS;
 }
