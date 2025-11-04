cdc62189b636d42927c0f7d7530510eab7ebcc7f
ash1606 <ashjun2005@gmail.com>
2025-11-05 00:14:28 +0530

    Modified frame.c

--- pintos/src/vm/frame.c
+++ pintos/src/vm/frame.c
@@ -152,7 +152,7 @@ frame_free (struct frame *f)
      Hint: You need to mark this frame as free again 
      so it can be reused by other pages.
      Do NOT remove the lock operations. */
-
+    f->page = NULL;
   lock_release (&f->lock);
 }
 
